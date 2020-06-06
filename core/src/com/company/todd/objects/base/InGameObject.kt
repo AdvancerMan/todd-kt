bb54e39ca0edf6edfb0ef479b8f21d836b090de6
AdvancerMan <advancerman@gmail.com>
2020-06-06 20:34:24 +0700

    Implemented GameScreen

---
+++ core/src/com/company/todd/objects/base/InGameObject.kt
@@ -0,0 +1,23 @@
+package com.company.todd.objects.base
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.utils.Disposable
+
+class InGameObject: Disposable {
+    fun preUpdate(delta: Float) {
+        TODO()
+    }
+
+    fun postUpdate(delta: Float) {
+        TODO()
+    }
+
+    fun draw(batch: SpriteBatch, cameraRect: Rectangle) {
+        TODO()
+    }
+
+    override fun dispose() {
+        TODO()
+    }
+}
--- core/src/com/company/todd/screen/GameScreen.kt
+++ core/src/com/company/todd/screen/GameScreen.kt
@@ -5,19 +5,30 @@ import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
 import com.company.todd.launcher.ToddGame
+import com.company.todd.objects.base.InGameObject
 
 open class GameScreen(game: ToddGame): MyScreen(game) {
-    val world: World = World(Vector2(0f, -10f), true)
+    val world = World(Vector2(0f, -10f), true)
+    private val objects = mutableListOf<InGameObject>()
+    private val justAddedObjects = mutableListOf<InGameObject>()
 
     override fun update(delta: Float) {
-        TODO("Not yet implemented")
+        objects.forEach { it.preUpdate(delta) }
+        world.step(delta, 10, 10)
+        objects.forEach { it.postUpdate(delta) }
+
+        justAddedObjects.forEach { objects.add(it) }
+        justAddedObjects.clear()
     }
 
-    override fun draw(batch: SpriteBatch, cameraRect: Rectangle) {
-        TODO("Not yet implemented")
-    }
+    override fun draw(batch: SpriteBatch, cameraRect: Rectangle) =
+            objects.forEach { it.draw(batch, cameraRect) }
 
     override fun dispose() {
-        TODO("Not yet implemented")
+        justAddedObjects.forEach { it.dispose() }
+        justAddedObjects.clear()
+        objects.forEach { it.dispose() }
+        objects.clear()
+        world.dispose()
     }
 }
