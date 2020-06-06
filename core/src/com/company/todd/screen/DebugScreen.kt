d9dfcc032c1af949e2ed90c50622f12a842a8096
AdvancerMan <advancerman@gmail.com>
2020-06-06 18:13:59 +0700

    Added GameScreen and implemented DebugScreen

---
+++ core/src/com/company/todd/screen/DebugScreen.kt
@@ -0,0 +1,30 @@
+package com.company.todd.screen
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
+import com.company.todd.launcher.ToddGame
+
+class DebugScreen(game: ToddGame): GameScreen(game) {
+    private val renderer = Box2DDebugRenderer()
+    private var pressedPlay = true
+
+    override fun render(delta: Float) {
+        update(delta)
+        renderer.render(null, getCameraProjectionMatrix())
+    }
+
+    override fun update(delta: Float) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
+            pressedPlay = !pressedPlay
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.Q) || Gdx.input.isKeyPressed(Input.Keys.W) || pressedPlay) {
+            super.update(delta)
+        }
+    }
+
+    override fun dispose() {
+        super.dispose()
+        renderer.dispose()
+    }
+}
---
+++ core/src/com/company/todd/screen/GameScreen.kt
@@ -0,0 +1,19 @@
+package com.company.todd.screen
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Rectangle
+import com.company.todd.launcher.ToddGame
+
+open class GameScreen(game: ToddGame): MyScreen(game) {
+    override fun update(delta: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun draw(batch: SpriteBatch, cameraRect: Rectangle) {
+        TODO("Not yet implemented")
+    }
+
+    override fun dispose() {
+        TODO("Not yet implemented")
+    }
+}
