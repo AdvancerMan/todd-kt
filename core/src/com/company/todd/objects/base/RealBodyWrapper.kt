a98308e1a64c4654bf6c69239cfe836239479051
AdvancerMan <advancerman@gmail.com>
2020-06-06 20:41:51 +0700

    Added null check for platform calls in toPix/toMeters functions

--- core/src/com/company/todd/objects/base/RealBodyWrapper.kt
+++ core/src/com/company/todd/objects/base/RealBodyWrapper.kt
@@ -8,10 +8,10 @@ const val pixInMeter = 30f
 fun Float.toPix() = this * pixInMeter
 fun Float.toMeters() = this / pixInMeter
 
-fun Vector2.toPix() = this.scl(pixInMeter)
-fun Vector2.toMeters() = this.scl(1 / pixInMeter)
+fun Vector2.toPix() = this.scl(pixInMeter)!!
+fun Vector2.toMeters() = this.scl(1 / pixInMeter)!!
 
-fun Matrix4.toPix() = this.scl(pixInMeter)
+fun Matrix4.toPix() = this.scl(pixInMeter)!!
 
 class RealBodyWrapper {
 }
