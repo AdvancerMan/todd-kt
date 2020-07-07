6284839e159c81e743dd6e1bdc3d49cf29220be9
Kazakov Mihail <advancerman@gmail.com>
2020-07-07 19:10:25 +0700

    Filled BodyWrapper interface with methods

--- core/src/com/company/todd/objects/base/BodyWrapper.kt
+++ core/src/com/company/todd/objects/base/BodyWrapper.kt
@@ -1,4 +1,31 @@
 package com.company.todd.objects.base
 
-class BodyWrapper {
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.*
+import com.company.todd.screen.GameScreen
+
+
+interface BodyWrapper {
+    fun init(gameScreen: GameScreen)
+
+    fun applyLinearImpulseToCenter(impulse: Vector2)
+
+    fun getPosition(): Vector2
+
+    fun setVelocity(v: Vector2)
+
+    fun setYVelocity(yVel: Float)
+
+    fun setXVelocity(xVel: Float)
+
+    fun setCenterPosition(x: Float, y: Float, resetLinearVelocity: Boolean = true)
+
+    fun setPosition(x: Float, y: Float, resetSpeed: Boolean = true)
+
+    fun setAngle(angle: Float, resetAngularVelocity: Boolean = true)
+
+    fun getAABB(): Rectangle
+
+    fun destroy(world: World)
 }
