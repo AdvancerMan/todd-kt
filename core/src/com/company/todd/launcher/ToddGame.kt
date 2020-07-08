dfef401076fd275bf0dbd79547ad13cfbd5abc19
Kazakov Mihail <advancerman@gmail.com>
2020-07-08 12:43:19 +0700

    Added TextureManager to ToddGame

--- core/src/com/company/todd/launcher/ToddGame.kt
+++ core/src/com/company/todd/launcher/ToddGame.kt
@@ -7,16 +7,19 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.company.todd.screen.RainbowScreen
+import com.company.todd.util.asset.texture.TextureManager
 
 lateinit var assetsFolder: String private set
 
 class ToddGame: ApplicationAdapter() {
     lateinit var batch: Batch private set
     lateinit var screenManager: ScreenManager private set
+    lateinit var textureManager: TextureManager private set
 
     override fun create() {
         batch = SpriteBatch()
         screenManager = ScreenManager(RainbowScreen(this, 4f, 5f))
+        textureManager = TextureManager()
         assetsFolder = if (Gdx.app.type == Application.ApplicationType.Desktop) "android/assets/" else ""
     }
 
@@ -30,6 +33,7 @@ class ToddGame: ApplicationAdapter() {
     override fun dispose() {
         screenManager.pause()
         screenManager.dispose()
+        textureManager.dispose()
         batch.dispose()
     }
 }
