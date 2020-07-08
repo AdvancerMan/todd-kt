1c6ec46cd6ed89abd275270db37b3f9439e9680d
Kazakov Mihail <advancerman@gmail.com>
2020-07-08 12:43:49 +0700

    Created MyContactListener

---
+++ core/src/com/company/todd/util/box2d/MyContactListener.kt
@@ -0,0 +1,33 @@
+package com.company.todd.util.box2d
+
+import com.badlogic.gdx.physics.box2d.Contact
+import com.badlogic.gdx.physics.box2d.ContactImpulse
+import com.badlogic.gdx.physics.box2d.ContactListener
+import com.badlogic.gdx.physics.box2d.Manifold
+import com.company.todd.objects.base.InGameObject
+
+private fun invokeForBoth(contact: Contact, f: InGameObject.(InGameObject) -> Unit) {
+    contact.fixtureA.body.userData.let { a ->
+        a as InGameObject
+        contact.fixtureB.body.userData.let { b ->
+            b as InGameObject
+
+            a.f(b)
+            b.f(a)
+        }
+    }
+}
+
+class MyContactListener: ContactListener {
+    override fun beginContact(contact: Contact) =
+            invokeForBoth(contact) { this.beginContact(it, contact) }
+
+    override fun endContact(contact: Contact) =
+            invokeForBoth(contact) { this.endContact(it, contact) }
+
+    override fun preSolve(contact: Contact, oldManifold: Manifold) =
+            invokeForBoth(contact) { this.preSolve(it, contact, oldManifold) }
+
+    override fun postSolve(contact: Contact, impulse: ContactImpulse) =
+            invokeForBoth(contact) { this.postSolve(it, contact, impulse) }
+}
