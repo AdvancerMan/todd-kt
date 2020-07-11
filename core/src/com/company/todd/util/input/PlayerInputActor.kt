ccbb219a35281808026b8198f0deb88220c6b030
Kazakov Mihail <advancerman@gmail.com>
2020-07-11 22:37:23 +0700

    Added some keys to PlayerInputActor

--- core/src/com/company/todd/util/input/PlayerInputActor.kt
+++ core/src/com/company/todd/util/input/PlayerInputActor.kt
@@ -20,17 +20,22 @@ class PlayerInputActor : Actor() {
                         Input.Keys.W -> isJumping = false
                         Input.Keys.A -> isMovingLeft = false
                         Input.Keys.D -> isMovingRight = false
+                        Input.Keys.UP -> isJumping = false
+                        Input.Keys.LEFT -> isMovingLeft = false
+                        Input.Keys.RIGHT -> isMovingRight = false
                         else -> return false
                     }
                     return true
                 }
 
                 override fun keyDown(event: InputEvent, keycode: Int): Boolean {
-                    println(keycode)
                     when (keycode) {
                         Input.Keys.W -> isJumping = true
                         Input.Keys.A -> isMovingLeft = true
                         Input.Keys.D -> isMovingRight = true
+                        Input.Keys.UP -> isJumping = true
+                        Input.Keys.LEFT -> isMovingLeft = true
+                        Input.Keys.RIGHT -> isMovingRight = true
                         else -> return false
                     }
                     return true
