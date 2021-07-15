772921d30378975dd8589b91bd5b281109b35b7a
Kazakov Mihail <advancerman@gmail.com>
2021-07-16 01:27:26 +0700

    Added more tags for logs

--- core/src/com/company/todd/util/files/FileLogger.kt
+++ core/src/com/company/todd/util/files/FileLogger.kt
@@ -4,44 +4,54 @@ import com.badlogic.gdx.ApplicationLogger
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.utils.Disposable
 import java.io.PrintWriter
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
+import kotlin.random.Random
 
 class FileLogger(fileName: String, private val innerLogger: ApplicationLogger? = null) : ApplicationLogger, Disposable {
     private val printer = PrintWriter(Gdx.files.local(fileName).writer(false, Charsets.UTF_8.toString()))
 
+    @Suppress("SimpleDateFormat")
+    private fun formatMessage(level: String, tag: String, message: String): String {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        return "[$level][${dateFormat.format(Calendar.getInstance().time)}][$tag] $message"
+    }
+
     override fun log(tag: String, message: String) {
-        printer.println("[$tag] $message")
+        printer.println(formatMessage("INFO", tag, message))
         printer.flush()
         innerLogger?.log(tag, message)
     }
 
     override fun log(tag: String, message: String, exception: Throwable) {
-        printer.println("[$tag] $message")
+        printer.println(formatMessage("INFO", tag, message))
         exception.printStackTrace(printer)
         printer.flush()
         innerLogger?.log(tag, message, exception)
     }
 
     override fun error(tag: String, message: String) {
-        printer.println("[$tag] $message")
+        printer.println(formatMessage("ERROR", tag, message))
         printer.flush()
         innerLogger?.error(tag, message)
     }
 
     override fun error(tag: String, message: String, exception: Throwable) {
-        printer.println("[$tag] $message")
+        printer.println(formatMessage("ERROR", tag, message))
         exception.printStackTrace(printer)
         printer.flush()
         innerLogger?.error(tag, message, exception)
     }
 
     override fun debug(tag: String, message: String) {
-        printer.println("[$tag] $message")
+        printer.println(formatMessage("DEBUG", tag, message))
         printer.flush()
         innerLogger?.debug(tag, message)
     }
 
     override fun debug(tag: String, message: String, exception: Throwable) {
-        printer.println("[$tag] $message")
+        printer.println(formatMessage("DEBUG", tag, message))
         exception.printStackTrace(printer)
         printer.flush()
         innerLogger?.debug(tag, message, exception)
