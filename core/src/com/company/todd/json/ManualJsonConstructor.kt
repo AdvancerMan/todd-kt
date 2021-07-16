1f7167262e053393f415cecc301f67fa8218b428
Kazakov Mihail <advancerman@gmail.com>
2021-07-16 15:58:47 +0700

    Added constructor functions to compile-time

--- core/src/com/company/todd/json/ManualJsonConstructor.kt
+++ core/src/com/company/todd/json/ManualJsonConstructor.kt
@@ -1,7 +1,7 @@
 package com.company.todd.json
 
 @Target(AnnotationTarget.FUNCTION)
-annotation class ManualJsonConstructor
+annotation class ManualJsonConstructor(val constructorName: String = "")
 
 object JsonDefaults {
     fun setDefault(key: String, value: Any?, map: MutableMap<String, Pair<Any?, Boolean>>): Any? {
--- core/src/com/company/todd/json/SerializationType.kt
+++ core/src/com/company/todd/json/SerializationType.kt
@@ -1,4 +1,4 @@
 package com.company.todd.json
 
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class SerializationType(val category: String, val type: String = "")
