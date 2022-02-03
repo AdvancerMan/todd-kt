7125ecc1231261a92494522306b1b29d2c5edc35
Kazakov Mihail <advancerman@gmail.com>
2022-02-03 22:15:07 +0300

    Fix `PlayerThinker` event spamming

--- core/src/io/github/advancerman/todd/gui/ButtonInputActor.kt
+++ core/src/io/github/advancerman/todd/gui/ButtonInputActor.kt
@@ -8,12 +8,13 @@ import io.github.advancerman.todd.asset.texture.ToddDrawable
 open class ButtonInputActor(
     jsonSettings: JsonValue,
     resources: Map<String, Map<String, ToddDrawable>>,
-    jsonName: String, private val onChangedEvent: (Boolean) -> Unit
+    jsonName: String,
+    private val onChangedEvent: (Boolean) -> Unit
 ) : ScreenInputActor<Button>(
     Button(resources[jsonName]!!["up"]!!, resources[jsonName]!!["down"]!!),
     jsonSettings[jsonName]
 ) {
     override fun changed(event: ChangeListener.ChangeEvent) {
-        onChangedEvent(actor.isPressed)
+        onChangedEvent(actor.clickListener.isPressed)
     }
 }
